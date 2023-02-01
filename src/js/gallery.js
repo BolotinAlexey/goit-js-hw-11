d2c7466634c1accdf393e476cdf573fb3214bd4f
Oleksii Bolotin <samkiral73@gmail.com>
2023-02-01 19:28:52 +0100

    added simpleLightbox

---
+++ src/js/gallery.js
@@ -0,0 +1,6 @@
+import SimpleLightbox from 'simplelightbox';
+
+/* options lightbox*/
+const options = {};
+const lightbox = new SimpleLightbox('.gallery a', options);
+export default function createGallery() {}
